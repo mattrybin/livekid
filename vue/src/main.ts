430f1f9c64644f4b14bc2625d5e010f5c23018fc
Matt Rybin <workmateuszrybin@gmail.com>
2022-11-26 06:58:13 +0100

    refactor: remove packages and comments

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue"
-import { VueQueryPlugin } from "vue-query"
 import App from "./App.vue"
 
-createApp(App).use(VueQueryPlugin).mount("#app")
+createApp(App).mount("#app")
--- vue/src/utils/trpc.ts
+++ vue/src/utils/trpc.ts
@@ -2,32 +2,6 @@ import { httpLink } from "@trpc/client"
 import { Router } from "../../../backend/router"
 import { useTRPC } from "use-trpc"
 
-// export const trpc = createTRPCProxyClient<AppRouter>({
-//   links: [
-//     httpBatchLink({
-//       url: "http://localhost:3100",
-//     }),
-//   ],
-// })
-
-// export default defineNuxtPlugin(() => {
-//   const client = createTRPCProxyClient<AppRouter>({
-//     links: [
-//       httpBatchLink({
-//         /**
-//          * If you want to use SSR, you need to use the server's full URL
-//          * @link https://trpc.io/docs/ssr
-//          **/
-//         url: "http://localhost:3000/api/trpc",
-//       }),
-//     ],
-//   })
-//   return {
-//     provide: {
-//       client,
-//     },
-//   }
-// })
 export const { useQuery } = useTRPC<Router>({
   client: {
     links: [
